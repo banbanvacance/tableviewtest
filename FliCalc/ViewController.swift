d38e1dfef7200a6a81341f199edea00b84f04a1a
板東慶一郎 <bandoukeiichirou@bandoukeiichirou-no-macbook-pro.local>
2017-01-05 01:05:08 +0900

    test

--- FliCalc/ViewController.swift
+++ FliCalc/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
+    var row:Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        flexibleTable.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
+    @IBAction func increaseRow(_ sender: AnyObject) {
+        row = row + 1
+        flexibleTable.reloadData() //
+    }
+    @IBOutlet weak var flexibleTable: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return row
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath) 
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
